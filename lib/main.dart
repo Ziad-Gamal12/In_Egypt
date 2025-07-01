b977acd37086e548aa9be2d5deb7cec54d0b1237
Ziad_Gamal12 <ziadgemy123@gmail.com>
2025-07-01 16:38:27 +0300

    changed app icon and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:in_egypt/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const InEgypt());
 }
 
 class InEgypt extends StatelessWidget {
   const InEgypt({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: 'Flutter Demo', home: Scaffold());
